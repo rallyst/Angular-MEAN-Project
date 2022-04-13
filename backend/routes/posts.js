99df7f7f24b6e779beeca1c8dce2e97b276fd16f
Roman <rallyshops@gmail.com>
2022-04-13 14:41:49 +0300

    section 7

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -67,7 +67,7 @@ router.put(
       content: req.body.content,
       imagePath:imagePath
     });
-    
+
     Post.updateOne({_id: req.params.id}, post).then(result => {
       res.status(200).json({ message: 'Update seccessful!'});
     });
@@ -75,13 +75,31 @@ router.put(
 );
 
 router.get('', (req, res, next) => {
-  Post.find().then(documents => {
-    res.status(200).json({
-      message: 'Posts fetched succesfully!',
-      posts: documents
-    });
-  });
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  let fetchedPosts;
+
+  if (pageSize && currentPage) {
+    postQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  postQuery
+    .then(documents => {
+      fetchedPosts = documents;
+      return Post.count();
+    })
+    .then(count => {
+      res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: fetchedPosts,
+        maxPosts: count
+      });
+    })
 });
+
+
 
 router.get('/:id', (req, res, next) => {
   Post.findById(req.params.id).then(post => {
