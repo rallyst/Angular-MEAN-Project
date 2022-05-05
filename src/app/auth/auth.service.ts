a80ef7686f70e1bbfa6461150e0dafe85d416d32
Roman <rallyshops@gmail.com>
2022-05-05 19:35:38 +0300

    added guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { AuthData } from './auth-data.model';
 
@@ -9,9 +10,13 @@ import { AuthData } from './auth-data.model';
 export class AuthService {
   private isAuthentificated = false;
   private token: string;
+  private tokenTimer: any;
   private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) { }
+
+  constructor(
+    private http: HttpClient,
+    private router: Router) { }
 
   getToken() {
     return this.token;
@@ -36,13 +41,20 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
-    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+    this.http
+      .post<{ token: string, expiresIn: number }>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000);
+          console.log(expiresInDuration)
           this.isAuthentificated = true;
           this.authStatusListener.next(true);
+          this.router.navigate(['/']);
         }
       })
   }
@@ -51,5 +63,7 @@ export class AuthService {
     this.token = null;
     this.isAuthentificated = false;
     this.authStatusListener.next(false);
+    this.router.navigate(['/']);
+    clearTimeout(this.tokenTimer);
   }
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -19,6 +19,8 @@ export class LoginComponent implements OnInit {
     if (form.invalid) {
       return;
     }
+
+    this.isLoading = true;
     this.authService.login(form.value.email, form.value.password);
   }
 }
