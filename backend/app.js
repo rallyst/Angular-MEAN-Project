d5f11b94055251a9b66d8666d59437a3df851d2b
Roman <rallyshops@gmail.com>
2022-05-13 11:43:51 +0300

    finished

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 mongoose
   .connect(
-    'mongodb://rallyshop:64rfcU0zjx7C2uFl@cluster0-shard-00-00.34aw4.mongodb.net:27017,cluster0-shard-00-01.34aw4.mongodb.net:27017,cluster0-shard-00-02.34aw4.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-m04609-shard-0&authSource=admin&retryWrites=true&w=majority'
+    'mongodb://rallyshop:'+ process.env.MONGO_ATLAS_PW +'@cluster0-shard-00-00.34aw4.mongodb.net:27017,cluster0-shard-00-01.34aw4.mongodb.net:27017,cluster0-shard-00-02.34aw4.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-m04609-shard-0&authSource=admin&retryWrites=true&w=majority'
   )
   .then(() => {
     console.log('Connected to database!');
@@ -21,7 +21,8 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/images', express.static(path.join('backend/images')))
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/', express.static(path.join(__dirname, 'udemy-angular')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -37,5 +38,8 @@ app.use((req, res, next) => {
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'udemy-angular', 'index.html'));
+});
 
 module.exports = app;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://newnodeangularip-env.eba-bxdu5zng.us-east-1.elasticbeanstalk.com/api'
 };
